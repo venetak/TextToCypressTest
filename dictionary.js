18a28c2276000e1cb643da0f4afb7a29cc9f4064
venetak <veneta.kochovska93@gmail.com>
2023-06-17 10:34:46 +0300

    Add parser ad dependency and improve build using TS

---
+++ dictionary.js
@@ -0,0 +1,9 @@
+module.exports = {
+    nouns: ['button', 'input', 'hello', 'something', 'value'],
+    verbs: ['be', 'have', 'click', 'type'],
+    conjunctions: ['and'],
+    prepositions: ['in', 'on'],
+    determiners: ['the'],
+    modalVerbs: ['should', 'must'],
+    adverbs: ['visible', 'stopped']
+}
